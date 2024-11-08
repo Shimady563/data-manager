71adebcbea4d8b6cb3402767393a01eade4c3f8b
Maksim Shiryaev <shimady563@gmail.com>
2024-11-08 22:18:25 +0300

    fix: change term field to terms in discipline model

--- manager/src/main/resources/db/changelog/v.1.0.0/script/demo-discipline-data.sql
+++ manager/src/main/resources/db/changelog/v.1.0.0/script/demo-discipline-data.sql
@@ -1,2 +1,2 @@
-INSERT INTO discipline (id, name, term, major_id)
-VALUES (nextval('discipline_id_seq'), 'name', 1, 1);
+INSERT INTO discipline (id, name, terms, major_id)
+VALUES (nextval('discipline_id_seq'), 'name', '{1, 2}', 1);
