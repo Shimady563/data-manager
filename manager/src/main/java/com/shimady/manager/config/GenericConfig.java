8c18ac065f8fff0277ab59cecd73fe05ab5fc283
Maksim Shiryaev <shimady563@gmail.com>
2024-11-08 22:19:34 +0300

    feat: configure model mapper

---
+++ manager/src/main/java/com/shimady/manager/config/GenericConfig.java
@@ -0,0 +1,19 @@
+package com.shimady.manager.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GenericConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(false);
+        return mapper;
+    }
+}
