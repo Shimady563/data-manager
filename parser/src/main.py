8bb25d8eb98b42aa5fc655bf9be971934831bfa3
Maksim Shiryaev <shimady563@gmail.com>
2024-11-24 22:13:01 +0300

    fix: add cors config to parser

--- parser/src/main.py
+++ parser/src/main.py
@@ -15,4 +15,16 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(excel_router.router)
